c51056b98fb962514ddb37f9e9c0271722b8afe2
BozzOneHiggs <jammz2607@gmail.com>
2025-09-17 14:15:53 +0200

    Wersja 0.1

--- app/src/main/java/com/example/smssenderservice/CustomTokenProvider.kt
+++ app/src/main/java/com/example/smssenderservice/CustomTokenProvider.kt
@@ -4,7 +4,7 @@ import android.content.Context
 
 object CustomTokenProvider {
 
-    private const val PLACEHOLDER_VALUE = ""
+    private const val PLACEHOLDER_VALUE = "SET_YOUR_CUSTOM_TOKEN_HERE"
 
     fun getToken(context: Context): String? {
         val token = context.getString(R.string.firebase_custom_token)
